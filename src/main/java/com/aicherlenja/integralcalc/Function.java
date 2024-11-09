b5a6454e84bb45f0a974d6b89f202f334f93915e
IStealMoney <IStealMoney@users.noreply.github.com>
2024-11-09 23:26:08 +0100

    added instructions

--- src/main/java/com/aicherlenja/integralcalc/Function.java
+++ src/main/java/com/aicherlenja/integralcalc/Function.java
@@ -2,4 +2,11 @@ package com.aicherlenja.integralcalc;
 
 public class Function {     // superclass
 
+    // Stammfunktion bilden (aufleiten)
+    //Hochzahl++
+    // a = a/neue Hochzahl
+
+
+    //Grenzen für x einsetzen (zuerst obere, dann untere (Subtraktion))
+
 }
