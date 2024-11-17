834f3ef81898b4fd0942954b484c09a5f758dbbb
IStealMoney <IStealMoney@users.noreply.github.com>
2024-11-17 15:26:58 +0100

    prevented NullPointerException, test for other device

--- src/main/java/com/aicherlenja/integralcalc/Troubleshooter.java
+++ src/main/java/com/aicherlenja/integralcalc/Troubleshooter.java
@@ -10,13 +10,13 @@ public class Troubleshooter {
         // x2 smaller than x1
         if(guiCon.uiX2 < guiCon.uiX1) {
             System.out.println("hola");
-            guiCon.displayErrorX1X2("Error: The value of X2 needs to be greater than the value of X1!");
+            //guiCon.displayErrorX1X2("Error: The value of X2 needs to be greater than the value of X1!");
             return false;
         }
 
         // too much entries for linear function
         if (guiCon.uiTextFieldC == 0) {
-            guiCon.displayErrorLinTooMuch("You just need to fill in values for a and b!");
+            //guiCon.displayErrorLinTooMuch("You just need to fill in values for a and b!");
             return false;
         }
 
