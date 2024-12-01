eb58f17c87d9cf0ecf9ee9faf2303f8e6eaa443c
IStealMoney <IStealMoney@users.noreply.github.com>
2024-12-01 18:39:23 +0100

    differs between functions with and without x

--- src/main/java/com/aicherlenja/integralcalc/CompositeFunction.java
+++ src/main/java/com/aicherlenja/integralcalc/CompositeFunction.java
@@ -6,23 +6,32 @@ import static com.aicherlenja.integralcalc.GUIController.uiX1;
 import static com.aicherlenja.integralcalc.GUIController.uiX2;
 
 public class CompositeFunction { // manages function components
-    public static float coefficient, exponent;
+    public static float coefficient, exponent, xVar;
 
     public static void separateFunction() {
         String[] functionSplit = GUIController.function.split("(?=[+-])");
 
         for (String s : functionSplit) {
-            s = s.trim();
+            //s = s.trim();
+            s = s.replaceAll("\\s", "");
 
             System.out.println("Verarbeite Summand: " + s);
             if (s.contains("x")) {
                 System.out.println("enthält x");
+                coefficient = Float.parseFloat(s.substring(0, s.indexOf("x")));
+                System.out.println("Coefficient: " + coefficient);
+                xVar = Float.parseFloat(s.substring(s.indexOf("x"), s.length()-1));
+                exponent = Float.parseFloat(s.substring(s.indexOf("x")+1, s.length()-1));
+                System.out.println("Exponent: " + exponent);
+            } else if (!s.contains("x")) {
+                System.out.println("Summand ohne x: " + s);
+                s = (s + "x");
+                System.out.println("Summand mit x: " + s);
+                coefficient = Float.parseFloat(s.substring(0, s.indexOf("x")));
+                System.out.println("Coefficient: " + coefficient);
             }
 
-            coefficient = Float.parseFloat(s.substring(0, s.indexOf("x")));
-            System.out.println("Coefficient: " + coefficient);
-            exponent = Float.parseFloat(s.substring(s.indexOf("x") + 1, s.length()));
-            System.out.println("Exponent: " + exponent);
+
 
             // calculate integral for specific function type
             switch (GUIController.selectedComboBox) {
